43db34b02af93b8bd6bf46068340da2bca257429
Iwan Timmer <irtimmer@gmail.com>
2019-03-26 20:23:48 +0100

    Unbind all device in iommu group for vfio plugin

--- pyqemu/plugins/vfio.py
+++ pyqemu/plugins/vfio.py
@@ -8,17 +8,18 @@ class Vfio:
 
   def startup(self, qemu):
     for device in self.devices:
-      data = open('/sys/bus/pci/devices/0000:%s/uevent'%(device['host'])).read()
-      conf = dict((line.split("=", 1) for line in data.splitlines()))
+      for host in os.listdir('/sys/bus/pci/devices/0000:%s/iommu_group/devices'%(device['host'])):
+        data = open('/sys/bus/pci/devices/%s/uevent'%(host)).read()
+        conf = dict((line.split("=", 1) for line in data.splitlines()))
 
-      if 'DRIVER' not in conf or conf['DRIVER'] != 'vfio-pci':
-        path = '/sys/bus/pci/devices/0000:%s/driver/unbind'%(device['host'])
-        if os.path.exists(path):
-          with open(path, 'w') as f:
-            f.write('0000:%s\n'%(device['host']))
+        if 'DRIVER' not in conf or conf['DRIVER'] != 'vfio-pci':
+          path = '/sys/bus/pci/devices/%s/driver/unbind'%(host)
+          if os.path.exists(path):
+            with open(path, 'w') as f:
+              f.write('%s\n'%(host))
 
-        with open('/sys/bus/pci/drivers/vfio-pci/new_id', 'w') as f:
-          f.write(conf['PCI_ID'].replace(':', ' '))
+          with open('/sys/bus/pci/drivers/vfio-pci/new_id', 'w') as f:
+            f.write(conf['PCI_ID'].replace(':', ' '))
 
   def destroy(self):
     for device in self.devices:
