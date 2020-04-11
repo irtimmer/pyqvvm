30fab5aaa231b107af54b0c6e00e4c37c02cae34
Iwan Timmer <irtimmer@gmail.com>
2020-04-11 17:11:04 +0200

    Fix hugepages plugin

--- pyqvmm/plugins/hugepages.py
+++ pyqvmm/plugins/hugepages.py
@@ -12,16 +12,16 @@ class Hugepages:
       size = int(f.read())
 
     with open('/proc/sys/vm/nr_hugepages', 'w') as f:
-      f.write(str(size + self.options.pages))
+      f.write(str(size + self.options['pages']))
 
   def destroy(self):
     size = 0
     with open('/proc/sys/vm/nr_hugepages', 'r') as f:
       size = int(f.read())
 
-    if size > self.options.pages:
+    if size > self.options['pages']:
       with open('/proc/sys/vm/nr_hugepages', 'w') as f:
-        f.write(str(size - self.options.pages))
+        f.write(str(size - self.options['pages']))
 
 def getInstance(options):
   return Hugepages(options)
