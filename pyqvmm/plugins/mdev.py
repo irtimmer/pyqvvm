a1916e6640a1b1dfc53b697fb3216251f6bcebe7
Iwan Timmer <irtimmer@gmail.com>
2020-04-11 16:54:36 +0200

    Add mdev plugin to create mediated devices

---
+++ pyqvmm/plugins/mdev.py
@@ -0,0 +1,18 @@
+# SPDX-License-Identifier: GPL-2.0-only
+
+class Mdev:
+  def __init__(self, options):
+    self.devices = options
+
+  def startup(self, qemu):
+    for device in self.devices:
+      with open('/sys/bus/pci/devices/%s/mdev_supported_types/%s/create'%(device['addr'], device['type']), 'w') as f:
+        f.write(device['uuid'])
+
+  def destroy(self):
+    for device in self.devices:
+      with open('/sys/bus/pci/devices/%s/%s/remove'%(device['addr'], device['uuid']), 'w') as f:
+        f.write('1')
+
+def getInstance(options):
+  return Mdev(options)
